a23061ecf6d6f83145c87b2c187bf53b5a79ded7
Iwo Polański <outlying@gmail.com>
2019-09-26 18:12:51 +0200

    Fix this and that

--- core/src/test/java/com/antyzero/ticket/core/repository/json/JsonFileRepositoryTest.kt
+++ core/src/test/java/com/antyzero/ticket/core/repository/json/JsonFileRepositoryTest.kt
@@ -9,7 +9,7 @@ import org.threeten.bp.LocalDate
 class JsonFileRepositoryTest {
 
     @Test
-    fun `serialize`() {
+    fun serialize() {
         val ticket = Ticket(
             id = "some ID",
             status = Ticket.Status.Valid(until = LocalDate.MIN),
@@ -17,13 +17,13 @@ class JsonFileRepositoryTest {
         )
         val adapter = JsonFileRepository.createAdapter()
 
-        val result = adapter.toJson(listOf(ticket)).also { println(it) }
+        val result = adapter.toJson(listOf(ticket))
 
         assertThat(result).isNotEmpty()
     }
 
     @Test
-    fun `deserialize`() {
+    fun deserialize() {
         val json =
             "[{\"id\":\"some ID\",\"statusType\":\"com.antyzero.ticket.core.model.Ticket\$Status\$Valid\",\"status\":{\"until\":{\"day\":1,\"month\":1,\"year\":-999999999}},\"dataType\":\"com.antyzero.ticket.core.utils.WierdoData\",\"data\":{\"name\":\"name\"}}]"
         val adapter = JsonFileRepository.createAdapter()
